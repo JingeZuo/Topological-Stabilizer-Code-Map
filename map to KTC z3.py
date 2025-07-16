2665db676f9e815499b8701d15baf3fcd02bd382
Jinge Zuo <202208108+JingeZuo@users.noreply.github.com>
2025-07-16 18:42:05 +0800

    Add files via upload

--- map to KTC z3.py
+++ map to KTC z3.py
@@ -1,7 +1,17 @@
 import numpy as np
 from z3 import *
 from functools import reduce
-
+def commute(a, b, init_val=False):
+    indices1 = [(i, (i+9) % 18) for i in (5,7,14,16)]
+    indices2 = [(i, (i+7) % 14) for i in (2,4,9,11)]
+    terms = []
+    for i, j in indices1:
+        if j < len(b):  # 确保不会越界
+            terms.append(And(a[i-1], b[j-1]))
+    for i,j in indices2:
+        if j < len(b):
+            terms.append(And(a[i - 1], b[j - 1]))
+    return reduce(Xor, terms, init_val)
 # 参数设置
 k = 16
 n = 16
@@ -10,16 +20,17 @@ target_add = []
 charstab=[
     [1, 3],
     [1, 2],
-    [9, 10],
-    [10, 12],
+    [5,7,10,12],
+    [5,6,9,10],
     [5, 6],
     [5, 7],
-    [14, 16],
-    [13, 14],
-[1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16]
+    [1,2,13,14],
+    [1,3,14,16],
+[1, 2, 3, 4,13,14,15,16],
+    [1,2,3,4],
+    [5,6,7,8,9, 10, 11, 12],
+    [5,6,7,8]
+
 ]
 s = Solver()
 for i in range(len(charstab)):
@@ -34,17 +45,17 @@ for i in range(n):
 for i in range(n):
     s.add(Xor(X[4][i], X[5][i]) == BoolVal(target_add[1][i]))
 for i in range(n):
-    s.add(Xor(X[8][i], X[9][i]) == BoolVal(target_add[2][i]))
+    s.add(Xor(X[8][i], X[10][i]) == BoolVal(target_add[2][i]))
 for i in range(n):
-    s.add(Xor(X[12][i], X[14][i]) == BoolVal(target_add[3][i]))
+    s.add(Xor(X[12][i], X[13][i]) == BoolVal(target_add[3][i]))
 for i in range(n):
     s.add(Xor(X[0][i], X[1][i]) == BoolVal(target_add[4][i]))
 for i in range(n):
     s.add(Xor(X[4][i], X[6][i]) == BoolVal(target_add[5][i]))
 for i in range(n):
-    s.add(Xor(X[8][i], X[10][i]) == BoolVal(target_add[6][i]))
+    s.add(Xor(X[8][i], X[9][i]) == BoolVal(target_add[6][i]))
 for i in range(n):
-    s.add(Xor(X[12][i], X[13][i]) == BoolVal(target_add[7][i]))
+    s.add(Xor(X[12][i], X[14][i]) == BoolVal(target_add[7][i]))
 for i in range(n):
     variables = [X[0][i], X[1][i], X[2][i], X[3][i]]
     xor_expr = reduce(Xor, variables)
@@ -64,12 +75,10 @@ for i in range(n):
 # 可选：添加点积约束 x1·x2 ≡ 1 mod 2
 for i in range(k):
     for j in range(k):
-        if i+8 == j:
-            dot_product = reduce(lambda a, b: Xor(a, b), [And(X[i][i], X[j][i]) for i in range(n)], False)
-            s.add(dot_product)
+        if j==(i+8)%16:
+            s.add(commute(X[i],X[j],init_val=False))
         else:
-            dot_product = reduce(lambda a, b: Xor(a, b), [And(X[i][i], X[j][i]) for i in range(n)], True)
-            s.add(dot_product)
+            s.add(commute(X[i],X[j],init_val=True))
 for i in range(k):
     for i in range(k):
         s.add(Not(And([Not(X[i][j]) for j in range(n)])))
@@ -88,35 +97,4 @@ if s.check() == sat:
     for i in range(k):
         print(f'x{i+1}=', sol[i])
 print(s.check())  # 看看是不是 unsat
-#%%
-
-k = 4
-n = 8
-X = [[Bool(f'x{i}_{j}') for j in range(n)] for i in range(k)]
-target_add = [1,1,1,0,1,0,1,0]
-s = Solver()
-for i in range(n):
-    variables = [X[0][i], X[1][i], X[2][i], X[3][i]]
-    xor_expr = reduce(Xor, variables)
-    s.add(xor_expr == BoolVal(target_add[i]))
-dot_product = reduce(lambda a, b: Xor(a, b), [And(X[0][i], X[1][i]) for i in range(n)], True)
-s.add(dot_product)
-for i in range(k):
-    for i in range(k):
-        s.add(Not(And([Not(X[i][j]) for j in range(n)])))
-for i in range(k):
-    for j in range(i + 1, k):
-        # 向量 X[i] 和 X[j] 不相等
-        s.add(Not(And([X[i][p] == X[j][p] for p in range(n)])))
-if s.check() == sat:
-    m = s.model()
-    def get_vector(model, vec):
-        return [1 if is_true(model.eval(v)) else 0 for v in vec]
-    sol = []
-    for i in range(k):
-        sol.append(get_vector(m, X[i]))
-    for i in range(k):
-        print(f'x{i+1}=', sol[i])
-print(s.check())  # 看看是不是 unsat
-
 
